167b77b97b81a12c75eddb0d22864446c96701ea
mGreb <mgreb92@gmail.com>
2014-01-31 12:32:29 +0600

    added task 6

---
+++ ProjectEuler/6/6.cpp
@@ -0,0 +1,33 @@
+﻿//Sum square difference
+//Problem 6
+//
+//The sum of the squares of the first ten natural numbers is,
+//1^2 + 2^2 + ... + 10^2 = 385
+//
+//The square of the sum of the first ten natural numbers is,
+//(1 + 2 + ... + 10)^2 = 552 = 3025
+//
+//Hence the difference between the sum of the squares of the first ten natural 
+//numbers and the square of the sum is 3025 − 385 = 2640.
+//
+//Find the difference between the sum of the squares of the first one hundred 
+//natural numbers and the square of the sum.
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+	double result = 0;
+	double sumOfSquares = 0;
+	double squareOfSum = 0;
+	for (int i = 1; i <= 100; i++){
+		sumOfSquares += i * i;
+		squareOfSum += i;
+	}
+	squareOfSum = squareOfSum * squareOfSum;
+	result = squareOfSum - sumOfSquares;
+	printf("squareOfSum: %lf\n", squareOfSum);
+	printf("sumOfSquares: %lf\n", sumOfSquares);
+	printf("Result is: %lf\n", result);
+	_getch();
+	return 0;
+}

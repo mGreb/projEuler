2afc03fe6e9efe063a2dca3ca842dd6b5c5f95ed
mGreb <oers92@mail.ru>
2014-05-19 23:11:29 +0600

    task 22 started

---
+++ ProjectEuler/22/22/22.c
@@ -0,0 +1,63 @@
+﻿//Names scores
+//Problem 22
+//
+//Using names.txt (right click and 'Save Link/Target As...'), a 46K text file
+//containing over five-thousand first names, begin by sorting it into
+//alphabetical order. Then working out the alphabetical value for each name,
+//multiply this value by its alphabetical position in the list to obtain a name
+//score.
+//
+//For example, when the list is sorted into alphabetical order, COLIN, which is
+//worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN
+//would obtain a score of 938 × 53 = 49714.
+//
+//What is the total of all the name scores in the file?
+
+#include <cstdio>
+
+const int size = 20;
+
+bool compare (char a[size], char b[size])
+{
+	bool AisBigger = true;
+	int i = 0;
+	for (i = 0; i < size; i++)
+	{
+		if (a[i]<b[i])
+		{
+			AisBigger = false;
+			return AisBigger;
+		}
+	}
+	return AisBigger;
+}
+
+struct name
+{
+	name* prevName;
+	name* nextName;
+	char thisIsName[size];
+};
+
+char* getName(const char* fileName)
+{
+	FILE *fp = fopen(fileName, "r");
+	char symbol;
+	char name[size];
+	int i = 0;
+	while (symbol != EOF)
+	{
+		symbol = fgetc(fp);
+		if (symbol != '"' || symbol != ',')
+		{
+		}
+		i++;
+	}
+	fclose(fp);
+	return 0;
+}
+
+int main()
+{
+	return 0;
+}

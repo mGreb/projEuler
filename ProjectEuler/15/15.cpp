6a8cdc20f71302fa916a222c7309f3d35b3172bc
mgreb <mgreb92@gmail.com>
2017-12-02 17:48:14 +0500

    Speedup for task 15.

--- ProjectEuler/15/15.cpp
+++ ProjectEuler/15/15.cpp
@@ -5,24 +5,49 @@
 //the right and down, there are exactly 6 routes to the bottom right corner.
 //
 //How many such routes are there through a 20×20 grid?
-#include <iostream>
-#include <cstdlib>
-using namespace std;
 
-double f(double row, double col) {
-	if(row == 0 && col == 0)
-		return 1.;
-	double result = 0;
-	if(row > 0)
-		result += f(row - 1, col);
-	if(col > 0)
-		result += f(row, col - 1);
-	return result;
+#include <iostream>
+#include <array>
+
+static const size_t dim = 20;
+std::array<std::array<size_t, dim>, dim> cache;
+
+size_t f(
+    const size_t row,
+    const size_t col
+){
+    size_t &cache_item = cache[row][col];
+    
+    if((row == 0) || (col == 0))
+    {
+        cache_item = 1;
+        return cache_item;
+    }
+    
+    if(cache_item != 0)
+    {
+        return cache_item;
+    }
+    else
+    {
+        cache_item = f(row - 1, col) + f(row, col - 1);
+    }
+    
+    return cache_item;
 }
 
-int main() {
-	double gridDim = 20;
-	cout.precision(20);
-	cout << "Result: " << f(gridDim, gridDim) << endl; 
-	system("pause"); 
+int main()
+{
+    for(size_t row = 0; row < cache.size(); ++row)
+    {
+        for(size_t col = 0; col < cache[row].size(); ++col)
+        {
+            cache[row][col] = 0;
+        }
+    }
+    
+    std::cout.precision(20);
+    std::cout << "Result: " << f(dim, dim) << std::endl;
+    
+    return 0;
 }

3712e6deb9a66f0e15bbad134020e7a47476b577
mgreb <mgreb92@gmail.com>
2022-03-14 20:13:24 +0500

    task 67 refactor

--- 0067/0067.cpp
+++ 0067/0067.cpp
@@ -21,88 +21,55 @@
 //	  over twenty billion years to check them all. There is an efficient 
 //	  algorithm to solve it. ;o)
 
-
-#include <stdio.h>
-#include <malloc.h>
-#include <stdlib.h>
-#include <conio.h>
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 int elemNum(int a)
 {
 	int res = 0;
 	for (int i = 1; i <= a; i++)
-	{
 		res += i;
-	}
+	
 	return res;
 }
 
-int printTriangle(int height, int* tree)
+int main()
 {
-	int k = 0;
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j <= i; j++)
-		{
-			printf("%d ", tree[k]);
-			k++;
-		}
-		printf("\n");
-	}
-	return 0;
-}
-
-int main(int argc, char const *argv[])
-{
-	const char* filePath = "67.txt";
-	const char* mode = "r+";
-	FILE* file;
-	fopen_s(&file, filePath, mode);
-	int readLength = 3;
-	char* buffer = (char*)calloc(readLength+1, sizeof(char));
-	int num = 0;//height of the triangle
-	fread(buffer, sizeof(char), readLength, file);
-	num = atoi(buffer);
-	int elemQuantity = elemNum(num);//total amount of elements in the 
+	std::ifstream stream;
+	stream.open("../../0067/triangle.txt");
+	int num; stream >> num;
+	const int elemQuantity = elemNum(num);  //total amount of elements in the triangle
 	printf("Height of the triangle: %d\n", num);
 	printf("Quantity of elements: %d\n", elemQuantity);
 	
-	int* arr = (int*)calloc(elemQuantity, sizeof(int));
+	std::vector<int> arr(elemQuantity);
 	for (int i = 0; i < elemQuantity; i++)
 	{
-		fread(buffer, sizeof(char), readLength, file);
-		arr[i] = atoi(buffer);
+		int val; stream >> val;
+		arr[i] = val;
 	}
 	
-	fclose (file);
-
-	printTriangle(num, arr);
-
-	//main count cycle
-	int firstRoute = 0;
-	int secondRoute = 0;
+	stream.close();
+	
 	int nextLayerStartPos =  elemQuantity - num;
 	int currLayerStartPos = nextLayerStartPos - num + 1;
 	for (int i = num - 1; i >= 0; i--)
 	{
 		for (int j = 0; j < i; j++)
 		{
-			firstRoute = arr[currLayerStartPos+j] + arr[nextLayerStartPos+j];
-			secondRoute = arr[currLayerStartPos+j] + arr[nextLayerStartPos+j+1];
+			const int firstRoute = arr[currLayerStartPos+j] + arr[nextLayerStartPos+j];
+			const int secondRoute = arr[currLayerStartPos+j] + arr[nextLayerStartPos+j+1];
 			if (firstRoute > secondRoute)
-			{
 				arr[currLayerStartPos+j] = firstRoute;
-			}
 			else
-			{
 				arr[currLayerStartPos+j] = secondRoute;
-			}
 		}
 		nextLayerStartPos =  currLayerStartPos;
 		currLayerStartPos = currLayerStartPos - i + 1;
 	}
-	printf("Maximum route weight is: %d\n", arr[0]);
-	_getch();
+	
+	std::cout << "Maximum route weight is: " << arr[0] << "\n";
+	
 	return 0;
 }
